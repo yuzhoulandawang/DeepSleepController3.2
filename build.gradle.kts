cdce6dcdf54ae40825697435b865e7d13338b6ef
yuzhoulandawang <814745511@qq.com>
2026-02-23 15:40:32 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    // 升级 Hilt 插件到 2.57
     id("com.google.dagger.hilt.android") version "2.57" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }
