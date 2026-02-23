7fe06742ddb4971f4410583690c2d257ed903e98
yuzhoulandawang <814745511@qq.com>
2026-02-23 11:30:07 +0800

    更新 CustomOptimizer.kt

--- app/src/main/java/com/example/deepsleep/utils/CustomOptimizer.kt
+++ app/src/main/java/com/example/deepsleep/utils/CustomOptimizer.kt
@@ -11,6 +11,7 @@ class CustomOptimizer {
             val commands = mutableListOf<String>()
             commands.addAll(OptimizationCommands.DeepDoze.forceIdle())
             commands.addAll(OptimizationCommands.CpuScheduler.applyDefaultMode())
+            // 直接传入列表
             commands.addAll(OptimizationCommands.BackgroundOptimizer.batchOptimizeApps(whitelist))
             RootCommander.execBatch(commands).isSuccess
         }
@@ -59,7 +60,10 @@ class CustomOptimizer {
         return withContext(Dispatchers.IO) {
             val commands = mutableListOf<String>()
             commands.addAll(OptimizationCommands.CpuScheduler.applyDefaultMode())
-            commands.addAll(OptimizationCommands.BackgroundOptimizer.batchOptimizeApps(whitelist))
+            // 遍历列表，传入单个应用包名
+            whitelist.forEach { app ->
+                commands.addAll(OptimizationCommands.BackgroundOptimizer.batchOptimizeApps(app))
+            }
             commands.addAll(OptimizationCommands.ScreenOptimizer.setBrightness(180))
             commands.addAll(OptimizationCommands.ScreenOptimizer.setAutoBrightness(false))
             RootCommander.execBatch(commands).isSuccess
