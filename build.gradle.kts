600102ef07aa7d54d39d41c6ff2c9eb65a173500
yuzhoulandawang <814745511@qq.com>
2026-02-23 13:30:12 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,4 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.53.1" apply false  // 新增 Hilt 插件版本
-}
-
-subprojects {
-    configurations.all {
-        resolutionStrategy {
-            force("com.squareup:javapoet:1.13.0")
-        }
-    }
 }
